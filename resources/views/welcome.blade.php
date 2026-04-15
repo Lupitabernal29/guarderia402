7d0e69fba0a4657f4c3ac2ab7f41b432a7fe2384
Lupita <almazanbernalcieloguadalupe@gmail.com>
2026-04-15 07:47:38 -0600

    guarderia402 uploaded

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,11 @@
+@extends("layouts.template")
+
+@section("content")
+    <div class="bg-body-tertiary p-5 rounded">
+        <h1>Bienvenido a la guarderia</h1>
+        <p class="lead">
+          GUARDERIA 402 YUPIIII!!!
+        </p>
+       
+      </div>
+@endsection
